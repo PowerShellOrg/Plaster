ee9106460c5dbf75f36185f11a660c3f8abaf797
Keith Hill <r_keith_hill@hotmail.com>
2016-10-06 00:06:58 -0600

    Too much duplication between example and NewModule
    Getting rid of the example NewModuleTemplate.  Don't need it anymore since we are working on an official NewModule template in the src\Templates\NewModule dir.  Not sure what we will have here but I believe it will be smaller, more focused examples.

--- examples/NewModuleTemplate/build.ps1
+++
@@ -1,8 +0,0 @@
-# This builds this module by invoking the build.psake.ps1 script
-
-if ($null -eq (Get-Module -Name PSake -ListAvailable)) {
-    throw "You need to install PSake before continuing. Install with 'Install-Module PSake -Scope CurrentUser'."
-}
-
-Import-Module Psake
-Invoke-PSake $PSScriptRoot\build.psake.ps1 -taskList Build
