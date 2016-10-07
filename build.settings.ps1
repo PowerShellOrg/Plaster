d4ebc29bbe397e0946ea75e29aac637afcdb1ceb
Keith Hill <r_keith_hill@hotmail.com>
2016-10-07 08:43:44 -0600

    Eliminate 28 script analyzer warnings.

--- build.settings.ps1
+++ build.settings.ps1
@@ -6,8 +6,10 @@ Properties {
     # ----------------------- Basic properties --------------------------------
 
     # The root directories for the module's docs, src and test.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='DocsRootDir')]
     $DocsRootDir = "$PSScriptRoot/docs"
     $SrcRootDir  = "$PSScriptRoot/src"
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='TestRootDir')]
     $TestRootDir = "$PSScriptRoot/test"
 
     # The name of your module should match the basename of the PSD1 file.
@@ -17,14 +19,17 @@ Properties {
 
     # The $OutDir must match the ModuleName in order to support publishing the module.
     $ReleaseDir = "$PSScriptRoot/Release"
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='OutDir')]
     $OutDir     = "$ReleaseDir/$ModuleName"
 
     # Default Locale used for documentation generatioon, defaults to en-US.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='DefaultLocale')]
     $DefaultLocale = $null
 
     # Items in the $Exclude array will not be copied to the $OutDir e.g. $Exclude = @('.gitattributes')
     # Typically you wouldn't put any file under the src dir unless the file was going to ship with
     # the module. However, if there are such files, add their $SrcRootDir relative paths to the exclude list.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='Exclude')]
     $Exclude = @()
 
 
@@ -34,6 +39,7 @@ Properties {
     # You can specify the certificate's subject name below. If not specified, you will be prompted to
     # provide either a subject name or path to a PFX file.  After this one time prompt, the value will
     # saved for future use and you will no longer be prompted.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='SignScripts')]
     $SignScripts = $false
 
     # Specify the Subject Name of the certificate used to sign your scripts.  Leave it as $null and the
@@ -46,6 +52,7 @@ Properties {
     #
     # Import-PfxCertificate -FilePath .\myCodeSigingCert.pfx -CertStoreLocation Cert:\CurrentUser\My
     #
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='CertSubjectName')]
     $CertSubjectName = $null
 
 
@@ -54,13 +61,16 @@ Properties {
     # Your NuGet API key for the PSGallery.  Leave it as $null and the first time you publish,
     # you will be prompted to enter your API key.  The build will store the key encrypted in the
     # settings file, so that on subsequent publishes you will no longer be prompted for the API key.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='NuGetApiKey')]
     $NuGetApiKey = $null
 
     # Name of the repository you wish to publish to. If $null is specified the default repo (PowerShellGallery) is used.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='PublishRepository')]
     $PublishRepository = $null
 
     # Path to the release notes file.  Set to $null if the release notes reside in the manifest file.
     # The contents of this file are used during publishing for the ReleaseNotes parameter.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='ReleaseNotesPath')]
     $ReleaseNotesPath = "$PSScriptRoot/ReleaseNotes.md"
 
 
@@ -69,18 +79,22 @@ Properties {
     # In addition, PFX certificates are supported in an interactive scenario only,
     # as a way to import a certificate into the user personal store for later use.
     # This can be provided using the CertPfxPath parameter. PFX passwords will not be stored.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='SettingsPath')]
     $SettingsPath = "$env:LOCALAPPDATA\Plaster\NewModuleTemplate\SecuredBuildSettings.clixml"
 
     # The local installation directory for the install task. Defaults to your user PSModulePath.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='InstallPath')]
     $InstallPath = $null
 
     # Specifies an output file path to send to Invoke-Pester's -OutputFile parameter.
     # This is typically used to write out test results so that they can be sent to a CI
     # system like AppVeyor.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='TestOutputFile')]
     $TestOutputFile = $null
 
     # Specifies the test output format to use when the TestOutputFile property is given
     # a path.  This parameter is passed through to Invoke-Pester's -OutputFormat parameter.
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='TestOutputFormat')]
     $TestOutputFormat = "NUnitXml"
 }
 
