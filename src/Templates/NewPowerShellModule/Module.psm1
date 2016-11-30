738f7188faf5174f9b54f21cfa08378f906a354e
Keith Hill <r_keith_hill@hotmail.com>
2016-11-29 23:51:50 -0700

    Add basic PowerShell module template.

---
+++ src/Templates/NewPowerShellModule/Module.psm1
@@ -0,0 +1,7 @@
+# Implement your module commands in this script.
+
+
+# Export only the functions using PowerShell standard verb-noun naming.
+# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
+# This improves performance of command discovery in PowerShell.
+Export-ModuleMember -Function *-*
---
+++ src/Templates/NewPowerShellModule/test/Module.T.ps1
@@ -0,0 +1,9 @@
+$ModuleManifestName = '<%=$PLASTER_PARAM_ModuleName%>.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath
+        $? | Should Be $true
+    }
+}
