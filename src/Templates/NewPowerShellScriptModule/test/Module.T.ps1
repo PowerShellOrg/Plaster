7bb5596d88fe2c89b2476f0bd8d334405ff6a27b
Keith Hill <r_keith_hill@hotmail.com>
2016-12-30 21:51:11 -0700

    Add extra check for ModuleManifest test.

--- src/Templates/NewPowerShellScriptModule/test/Module.T.ps1
+++ src/Templates/NewPowerShellScriptModule/test/Module.T.ps1
@@ -3,7 +3,7 @@ $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
-        Test-ModuleManifest -Path $ModuleManifestPath
+        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
         $? | Should Be $true
     }
 }
