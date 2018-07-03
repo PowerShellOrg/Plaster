71d07d9f9a3615da262b4fe08118760da59c55f2
Justin Grote <JustinGrote@users.noreply.github.com>
2018-07-02 21:07:03 -0700

    Add -ListAvailable param to Get-PlasterTemplate
    On the heels of #336, provide additional clarification and a parameter for "classic" operation due to slightly breaking change.

--- src/GetPlasterTemplate.ps1
+++ src/GetPlasterTemplate.ps1
@@ -36,10 +36,15 @@ function Get-PlasterTemplate {
         [Parameter(Position=0,
                    Mandatory=$true,
                    ParameterSetName="IncludedTemplates",
-                   HelpMessage="Initiates a search for Plaster templates inside of installed modules.")]
+                   HelpMessage="Initiates a search for Plaster templates inside of installed modules. By default, this retrieves from the latest version available for each module.")]
         [switch]
         [Alias("IncludeModules")]
-        $IncludeInstalledModules
+        $IncludeInstalledModules,
+        
+        [Parameter(ParameterSetName="Path",
+                   HelpMessage="If specified, searches for Plaster templates inside of all installed module versions. This may return multiple templates for each module.")]
+        [switch]
+        $ListAvailable
     )
 
     process {
@@ -92,7 +97,13 @@ function Get-PlasterTemplate {
 
             if ($IncludeInstalledModules.IsPresent) {
                 # Search for templates in module path
-                $extensions = Get-ModuleExtension -ModuleName Plaster -ModuleVersion $PlasterVersion
+                $GetModuleExtensionParams = @{
+                    ModuleName = "Plaster"
+                    ModuleVersion = $PlasterVersion
+                    ListAvailable = $ListAvailable
+                }
+                
+                $extensions = Get-ModuleExtension @GetModuleExtensionParams
 
                 foreach ($extension in $extensions) {
                     # Scan all module paths registered in the module
