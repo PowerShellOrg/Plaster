33a80ad145e12dd15d9578ac2268697e998f95e3
Gilbert Sanchez <me@gilbertsanchez.com>
2025-07-14 20:00:12 -0700

    feat: ✨ Enhance documentation for `Invoke-PlasterOperation` and `Write-PlasterLog` functions
    * Added detailed synopsis, description, parameters, examples, and notes to improve clarity and usability.
    * Ensured consistent logging and error handling across operations within the Plaster module.

--- Plaster/Private/Write-PlasterLog.ps1
+++ Plaster/Private/Write-PlasterLog.ps1
@@ -1,4 +1,33 @@
 function Write-PlasterLog {
+    <#
+    .SYNOPSIS
+    Logs messages with different severity levels for Plaster operations.
+
+    .DESCRIPTION
+    This function logs messages with different severity levels for Plaster
+    operations.
+
+    .PARAMETER Level
+    The severity level of the log message. Possible values are 'Error',
+    'Warning', 'Information', 'Verbose', and 'Debug'. The log message will be
+    formatted with a timestamp and the source of the log.
+
+    .PARAMETER Message
+    The message to log.
+
+    .PARAMETER Source
+    The source of the log message.
+
+    .EXAMPLE
+    Write-PlasterLog -Level 'Information' -Message 'This is an informational message.'
+
+    This example logs an informational message with the specified level and
+    source.
+    .NOTES
+    This function is designed to be used within the Plaster module to ensure
+    consistent logging across various operations.
+    It is not intended for direct use outside of the Plaster context.
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
